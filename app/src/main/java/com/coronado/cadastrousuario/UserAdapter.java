11051f6309bb535ddf2de8b9b86caec11a6f7ee0
Matheus Coronado <matheuscoronado48@gmail.com>
2026-03-26 19:31:14 -0300

    Criado a classe e o construtor UserAdapter

--- app/src/main/java/com/coronado/cadastrousuario/UserAdapter.java
+++ app/src/main/java/com/coronado/cadastrousuario/UserAdapter.java
@@ -1,4 +1,16 @@
 package com.coronado.cadastrousuario;
 
-public class UserAdapter {
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+// Classe Adapter: Gerencia a criação e o preenchimento dos itens na RecyclerView
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    // Atributo privado que armazena a referencia dos dados que serão exibidos
+    private List<String> ListaUsuarios;
+
+    // Construtor que permite que a MainActivity "entregue" a lista de dados para este Adapter
+    public UserAdapter(List<String> lista){
+        this.listaUsuarios = Lista;
+    }
 }
