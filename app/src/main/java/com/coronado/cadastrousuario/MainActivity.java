dfda8f8c029c76d514d7399b32ed9312a847d930
Matheus Coronado <matheuscoronado48@gmail.com>
2026-03-26 21:15:14 -0300

    Criação do atributo estatico

--- app/src/main/java/com/coronado/cadastrousuario/MainActivity.java
+++ app/src/main/java/com/coronado/cadastrousuario/MainActivity.java
@@ -10,11 +10,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     // Declarações dos componentes visuais e do adaptador da lista
     RecyclerView recyclerView;
     UserAdapter adapter;
     Button btnCadastrar;
 
+    // Atenção: Atributo estatico(stratic) permite que os dados persistem na memória
+    // Enquanto o app estiver aberto e sejam acessados diretamente por outras telas (Activities)
+    public static List<String> listaNomes = new ArrayList<>();
 
 }
