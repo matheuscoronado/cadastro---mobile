23882a0b058c45d5c04fbe3cdad4976f751c41d0
Matheus Coronado <matheuscoronado48@gmail.com>
2026-03-26 21:10:20 -0300

    Declarações dos componentes visuais e do adaptador da lista

--- app/src/main/java/com/coronado/cadastrousuario/MainActivity.java
+++ app/src/main/java/com/coronado/cadastrousuario/MainActivity.java
@@ -1,24 +1,20 @@
 package com.coronado.cadastrousuario;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    // Declarações dos componentes visuais e do adaptador da lista
+    RecyclerView recyclerView;
+    UserAdapter adapter;
+    Button btnCadastrar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+
 }
