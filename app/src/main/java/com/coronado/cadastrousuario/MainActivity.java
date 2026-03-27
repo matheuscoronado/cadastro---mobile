3de07868ae56f77dd3ec2b331a2f5d4f239bdd13
Matheus Coronado <matheuscoronado48@gmail.com>
2026-03-26 21:26:57 -0300

    Metodo de inicialização da Activity (Ponto de entrada da tela)

--- app/src/main/java/com/coronado/cadastrousuario/MainActivity.java
+++ app/src/main/java/com/coronado/cadastrousuario/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -22,5 +23,25 @@ public class MainActivity extends AppCompatActivity {
     // Atenção: Atributo estatico(stratic) permite que os dados persistem na memória
     // Enquanto o app estiver aberto e sejam acessados diretamente por outras telas (Activities)
     public static List<String> listaNomes = new ArrayList<>();
+
+    // Metodo de inicialização da Activity (Ponto de entrada da tela)
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // Vincula o arquivo de layout XML (activity_main.xml) a essa classe java
+        setContentView(R.layout.activity_main);
+
+        // Regra de negocio: Insere um texto no topo da lista, caso ela esteja vazia
+        if (listaNomes.isEmpty()){
+            listaNomes.add("Nomes de cadastro");
+        }
+
+        // Mapeamento dos componentes do RecyclerView do XML para o objeto java
+        recyclerView = findViewById(R.id.recycler_view);
+
+        // Define o LayoutManager: organiza itens da lista em uma coluna vertical simples
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 
 }
