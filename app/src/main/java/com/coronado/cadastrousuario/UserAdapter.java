6e6d3c64b13709f597339444ff000a66c94027ff
Matheus Coronado <matheuscoronado48@gmail.com>
2026-03-26 21:02:43 -0300

    Correção no metodo 2. Retirada do recyclerView

--- app/src/main/java/com/coronado/cadastrousuario/UserAdapter.java
+++ app/src/main/java/com/coronado/cadastrousuario/UserAdapter.java
@@ -36,7 +36,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     // Metodo 2: Vincula os dados de um objeto de lista a uma linha especifica da tela
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position){
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position){
         // Recupera os dados da lista de acordo com a posição que o Android está desenhando agora
         String nome = listaUsuarios.get(position);
 
